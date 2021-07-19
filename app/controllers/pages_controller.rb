2fde84d52454979c49bfe510ae355333e5cf3291
Guillaume Negro <negro.guillaume@gmail.com>
2021-07-19 17:04:56 +0200

    kite model, controller and seed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @sport_lvl = ["Beginner", "regular", "athletic"]
+    @weights = ["< 55 kg", "55-70 kg", "70-80 kg", "80-95 kg", "> 95 kg"]
+    @winds = ["no idea", "12 to 18 knots", "18 to 25 knots", "12 to 25 knots"]
   end
 end
