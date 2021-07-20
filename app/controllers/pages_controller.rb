e1cba9ec04e4e82db4fd4d3eda85b4767d2733d9
Guillaume Negro <negro.guillaume@gmail.com>
2021-07-20 11:25:31 +0200

    huge work on front search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   def home
     @sport_lvl = ["Beginner", "regular", "athletic"]
     @weights = ["< 55 kg", "55-70 kg", "70-80 kg", "80-95 kg", "> 95 kg"]
-    @winds = ["no idea", "12 to 18 knots", "18 to 25 knots", "12 to 25 knots"]
+    @winds = ["I have no idea", "12 to 18 knots - Good for begginer", "18 to 25 knots", "12 to 25 knots"]
   end
 end
