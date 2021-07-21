c6875f30be1a3ccd06abc7b87591ca1b1d8eb945
Guillaume Negro <negro.guillaume@gmail.com>
2021-07-21 14:48:29 +0200

    some seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,15 @@ duotone = Brand.create(name: "Duotone", logo_url: "https://www.duotonesports.com
 
 puts "Creating some kite"
 moto = Kite.create(model: "Moto", photo_url: "https://media1.vagueetvent.com/64098-large_default/kite-cabrinha-moto-2021.jpg", 
-    description: "A small description about this kite", brand_id: cabrinha.id)
+    description: "Moto is THE crossover kite of Cabrinha. Very Nice from Beginner to Expert that want One kite for all Practice", brand_id: cabrinha.id)
 switchblade = Kite.create(model: "Switchblade", photo_url: "https://media3.vagueetvent.com/64105-large_default/kite-cabrinha-switchblade-2021.jpg", 
-    description: "A small description about this kite", brand_id: cabrinha.id)
+    description: "Switchblade ... we don't need more details. Everyone know the switchblade. It is the most famous kite of Cabrinha range. Very good in Freeride, it will suit you a long time.", brand_id: cabrinha.id)
 bandit = Kite.create(model: "Bandit", photo_url: "https://fr.f-one.world/app/uploads/2020/08/BANDIT-2021-PAPAYA-SLATE-HD-650x650.png", 
-    description: "A small description about this kite", brand_id: fone.id)
+    description: "Since the the beginning, F-one has focused his work on create One kite that works in all conditions. This is the bandit.", brand_id: fone.id)
 banditS = Kite.create(model: "Bandit S", photo_url: "https://media2.vagueetvent.com/46177-large_default/kite-f-one-bandit-s2-2021.jpg", 
     description: "A small description about this kite", brand_id: fone.id)
 rs = Kite.create(model: "RS", photo_url: "https://media3.vagueetvent.com/70766-large_default/kite-eleveight-rs-v5-2022.jpg", 
-    description: "A small description about this kite", brand_id: eleveight.id)
+    description: "Most versatile kite of Eleveight range", brand_id: eleveight.id)
 evo = Kite.create(model: "Evo", photo_url: "https://media3.vagueetvent.com/51167-large_default/kite-duotone-evo-2021.jpg", 
     description: "A small description about this kite", brand_id: duotone.id)
 rebel = Kite.create(model: "Rebel", photo_url: "https://media2.vagueetvent.com/48542-large_default/kite-duotone-rebel-2021.jpg", 
