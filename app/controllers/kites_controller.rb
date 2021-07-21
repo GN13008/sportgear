70b44ac99051ed9d88c4d9b45853631c171135c4
Guillaume Negro <negro.guillaume@gmail.com>
2021-07-21 12:24:04 +0200

    some change on the introduction

--- app/controllers/kites_controller.rb
+++ app/controllers/kites_controller.rb
@@ -2,11 +2,11 @@ class KitesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :search ]
 
   def search
-    @usr_lvl = "YOOOOOOOO"
-    @usr_weight = "YOOOOOOOO"
+    @usr_lvl = "You did not say your lvl in other watersports / boardsports ! Here are all the kite we could recommend to a Beginner 🔥"
+    @usr_weight = ""
     @kites = Kite.all
     if params[:sport_lvl].present?
-      @usr_lvl = params[:sport_lvl]
+      @usr_lvl = "You say that you have a #{params[:sport_lvl]} lvl in other watersports / boardsports ? It's why we have select some gear that could suit you 🔥"
       accessibility = 1
       accessibility = 7 if @usr_lvl == "Beginner"
       @search_kites = @kites.select do |kite|
@@ -14,6 +14,9 @@ class KitesController < ApplicationController
       end
       @kites = @search_kites
     end
-    @usr_weight = params[:weights]
+    if params[:weights].present?
+      @usr_weight = " of #{params[:weights]}"
+    end
+    
   end
 end
