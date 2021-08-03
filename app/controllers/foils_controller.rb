26faa45275b2f05a1e1c74abdfd7eedeb967c02e
Guillaume Negro <negro.guillaume@gmail.com>
2021-08-03 09:45:50 +0200

    adding kitefoil and wingfoil

---
+++ app/controllers/foils_controller.rb
@@ -0,0 +1,100 @@
+class FoilsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :wing_search, :kite_search, :show ]
+
+  def wing_search
+    @usr_lvl = "You did not say your lvl in other watersports / boardsports ! Here are all the kite we could recommend to a Beginner 🔥"
+    @usr_weight = "12 and 9 are the standard quiver but you should fill your weights to have more relevant suggestions !"
+    @kites = Kite.all
+    @size_range_12_19 = 12
+    @size_range_19_25 = 9
+    if params[:sport_lvl].present?
+      if params[:sport_lvl] == "Beginner"
+        @usr_lvl = "You say that you have a #{params[:sport_lvl]} lvl in other watersports / boardsports ? This selection is fully dedicated to your lvl ! Enjoy 🔥"
+      else
+        @usr_lvl = "You say that you have a #{params[:sport_lvl]} lvl in other watersports / boardsports ? It's why we have select some gear that could suit you and that you could keep for a longue time 🔥"
+      end
+      accessibility = accessibility_filter(params[:sport_lvl])
+      @search_kites = @kites.select do |kite|
+        kite if kite.kite_parameters[0].rate > accessibility
+      end
+      @kites = @search_kites
+    end
+    if params[:weights].present?
+      size_factor = size_finder(params[:weights], params[:sport_lvl])
+      @size_range_12_19 = 12 + size_factor
+      @size_range_19_25 = 9 + size_factor
+      @usr_weight = "As a rider of #{params[:weights]}, you will need a #{@size_range_12_19} and a #{@size_range_19_25} !"
+    end
+
+  end
+
+  def kite_search
+    @usr_lvl = "You did not say your lvl in other watersports / boardsports ! Here are all the kite we could recommend to a Beginner 🔥"
+    @usr_weight = "12 and 9 are the standard quiver but you should fill your weights to have more relevant suggestions !"
+    @kites = Kite.all
+    @size_range_12_19 = 12
+    @size_range_19_25 = 9
+    if params[:sport_lvl].present?
+      if params[:sport_lvl] == "Beginner"
+        @usr_lvl = "You say that you have a #{params[:sport_lvl]} lvl in other watersports / boardsports ? This selection is fully dedicated to your lvl ! Enjoy 🔥"
+      else
+        @usr_lvl = "You say that you have a #{params[:sport_lvl]} lvl in other watersports / boardsports ? It's why we have select some gear that could suit you and that you could keep for a longue time 🔥"
+      end
+      accessibility = accessibility_filter(params[:sport_lvl])
+      @search_kites = @kites.select do |kite|
+        kite if kite.kite_parameters[0].rate > accessibility
+      end
+      @kites = @search_kites
+    end
+    if params[:weights].present?
+      size_factor = size_finder(params[:weights], params[:sport_lvl])
+      @size_range_12_19 = 12 + size_factor
+      @size_range_19_25 = 9 + size_factor
+      @usr_weight = "As a rider of #{params[:weights]}, you will need a #{@size_range_12_19} and a #{@size_range_19_25} !"
+    end
+
+  end
+
+  def show
+    @kite = Kite.find(params[:id])
+  end
+
+  private
+
+  def accessibility_filter(lvl)
+    # accessibility 1 Good Kiter only to 10 Beginner
+    case lvl
+    when "Beginner"
+      accessibility = 7
+    when "Regular"
+      accessibility = 5
+    when "Athletic"
+      accessibility = 1
+    else
+      accessibility = 0
+    end
+
+    return accessibility
+  end
+
+  def size_finder(weights, lvl)
+    size_factor = 0
+    lvl = "Beginner" if lvl == ""
+    size_factor -= 1 if lvl == "Beginner"
+    case weights
+    when "< 55 kg"
+      size_factor -= 3
+    when "55-65 kg"
+      size_factor -= 2
+    when "65-75 kg"
+      size_factor -= 1
+    when "75-85 kg"
+      # this is my reference to choose kite size
+    when "85-95 kg"
+      size_factor += 1
+    when "> 95 kg"
+      size_factor += 2
+    end
+    return size_factor
+  end
+end
--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -3,7 +3,29 @@ class SportsController < ApplicationController
 
   def show
     @sport = Sport.find(params[:id])
-    @sport_lvl = ["Beginner", "Regular", "Athletic"]
+    @sport_lvl = traduction_sport_lvl(params[:locale])
     @weights = ["< 55 kg", "55-65 kg", "65-75 kg", "75-85 kg", "85-95 kg", "> 95 kg"]
+    @path = path_selector(@sport.name)
+  end
+
+  private
+
+  def path_selector(sport)
+    case sport
+    when "Kitesurf"
+      kitesurf_search_path
+    when "Kitefoil"
+      kitefoil_search_path
+    when "Wingfoil"
+      wingfoil_search_path
+    end
+  end
+
+  def traduction_sport_lvl(lg)
+    if lg == "fr"
+      ["Débutant", "A l'aise", "Pro"]
+    else
+      ["Beginner", "Regular", "Athletic"]
+    end
   end
 end
